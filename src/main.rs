f4a2b52a9033a0de7c042e45d539a404109dbfc6
Yuki Sireneva <yuki.utk8g@gmail.com>
2024-08-20 23:01:22 +0300

    Add basic multi-monitor support

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,14 @@
-use anyhow::{bail, Context, Result};
+use anyhow::{bail, ensure, Context, Result};
 use chrono::{offset::Local, DateTime};
 use gtk::prelude::*;
 use gtk::{gdk, glib};
 use gtk4_layer_shell::LayerShell;
 use relm4::prelude::*;
 use smol::stream::StreamExt;
+use std::collections::{HashMap, HashSet};
 use std::time::Duration;
 
-#[derive(Debug, PartialEq)]
+#[derive(Debug, Clone, PartialEq)]
 struct XkbLayout {
     name: String,
     description: String,
@@ -15,8 +16,9 @@ struct XkbLayout {
 
 struct AppModel;
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 enum AppInput {
+    Outputs(HashSet<String>),
     Layout(XkbLayout),
     Time(DateTime<Local>),
 }
@@ -54,18 +56,12 @@ impl Component for AppModel {
     fn init(
         params: Self::Init,
         root: Self::Root,
-        sender: ComponentSender<Self>,
+        _sender: ComponentSender<Self>,
     ) -> ComponentParts<Self> {
         let monitor = params;
 
         let model = AppModel;
 
-        // TODO move to "opener"
-        let input_sender = sender.input_sender();
-        relm4::spawn(sway_state_listener(input_sender.clone()));
-        relm4::spawn(time_updater(input_sender.clone()));
-
-        // Insert the macro code generation here
         let widgets = view_output!();
 
         ComponentParts { model, widgets }
@@ -84,15 +80,18 @@ impl Component for AppModel {
                 ui.date.set_text(&time.format("%b %-d, %a").to_string());
                 ui.time.set_text(&time.format("%T").to_string());
             }
+            AppInput::Outputs(_) => {}
         }
     }
 }
 
-async fn time_updater(tx: relm4::Sender<AppInput>) -> Result<()> {
+async fn time_updater(tx: smol::channel::Sender<AppInput>) -> Result<()> {
     let mut timer = smol::Timer::interval(Duration::from_secs(1));
 
     loop {
-        tx.emit(AppInput::Time(Local::now()));
+        tx.send(AppInput::Time(Local::now()))
+            .await
+            .context("send time")?;
 
         if timer.next().await.is_none() {
             break;
@@ -102,18 +101,20 @@ async fn time_updater(tx: relm4::Sender<AppInput>) -> Result<()> {
     Ok(())
 }
 
-async fn sway_state_listener(tx: relm4::Sender<AppInput>) -> Result<()> {
+async fn sway_state_listener(tx: smol::channel::Sender<AppInput>) -> Result<()> {
     use swayipc_async::{Connection, Event, EventType};
 
     let mut conn = Connection::new().await.context("initial connection")?;
     let mut stream = Connection::new()
         .await
         .context("event connection")?
-        .subscribe([EventType::Input])
+        .subscribe([EventType::Input, EventType::Output])
         .await
         .context("subscribe to events")?;
 
-    // Initial state
+    sway_fetch_output(&tx, &mut conn)
+        .await
+        .context("init output")?;
     sway_fetch_input(&tx, &mut conn)
         .await
         .context("init input")?;
@@ -124,6 +125,9 @@ async fn sway_state_listener(tx: relm4::Sender<AppInput>) -> Result<()> {
             Event::Input(_) => sway_fetch_input(&tx, &mut conn)
                 .await
                 .context("fetch input")?,
+            Event::Output(_) => sway_fetch_output(&tx, &mut conn)
+                .await
+                .context("fetch output")?,
             _ => bail!("Unexpected event"),
         }
     }
@@ -132,7 +136,7 @@ async fn sway_state_listener(tx: relm4::Sender<AppInput>) -> Result<()> {
 }
 
 async fn sway_fetch_input(
-    tx: &relm4::Sender<AppInput>,
+    tx: &smol::channel::Sender<AppInput>,
     conn: &mut swayipc_async::Connection,
 ) -> Result<()> {
     let inputs = conn.get_inputs().await.context("get inputs")?;
@@ -141,36 +145,96 @@ async fn sway_fetch_input(
         .iter()
         .find_map(|input| input.xkb_active_layout_name.as_ref());
 
-    tx.emit(AppInput::Layout(XkbLayout {
+    tx.send(AppInput::Layout(XkbLayout {
         name: layout_name
             .map(|layout| layout[..2].to_ascii_lowercase())
             .unwrap_or_else(|| "xx".into()),
         description: layout_name
             .cloned()
             .unwrap_or_else(|| "Unknown layout".into()),
-    }));
+    }))
+    .await
+    .context("send layout")?;
+    Ok(())
+}
+
+async fn sway_fetch_output(
+    tx: &smol::channel::Sender<AppInput>,
+    conn: &mut swayipc_async::Connection,
+) -> Result<()> {
+    let outputs = conn
+        .get_outputs()
+        .await
+        .context("get outputs")?
+        .into_iter()
+        .map(|out| out.name)
+        .collect::<HashSet<_>>();
+
+    tx.send(AppInput::Outputs(outputs))
+        .await
+        .context("send outputs")?;
+
+    // sway_fetch_workspaces
+
     Ok(())
 }
 
 async fn main_loop(app: gtk::Application) -> Result<()> {
-    let output = "VGA-1".to_owned();
-    let monitor = gdk::Display::default()
-        .context("Failed to get default display")?
-        .monitors()
-        .into_iter()
-        .take_while(Result::is_ok)
-        .flatten()
-        .flat_map(|res| res.downcast::<gdk::Monitor>())
-        .find(|monitor| monitor.connector().as_deref() == Some(&output))
-        .context("Failed to find monitor for output")?;
+    let (tx, rx) = smol::channel::unbounded();
 
-    let mut controller = AppModel::builder().launch(monitor).detach();
-    let window = controller.widget();
-    app.add_window(window);
-    window.set_visible(true);
-    controller.detach_runtime();
+    relm4::spawn_local(sway_state_listener(tx.clone()));
+    relm4::spawn_local(time_updater(tx.clone()));
 
-    Ok(())
+    let mut windows: HashMap<String, Controller<AppModel>> = HashMap::new();
+
+    loop {
+        let event = rx.recv().await.context("receive event")?;
+        match event {
+            AppInput::Outputs(new_outputs) => {
+                windows.retain(|output, _| new_outputs.contains(output));
+
+                let monitors = gdk::Display::default()
+                    .context("Failed to get default display")?
+                    .monitors()
+                    .into_iter()
+                    .take_while(Result::is_ok)
+                    .flatten()
+                    .flat_map(|res| res.downcast::<gdk::Monitor>())
+                    .collect::<Vec<_>>();
+
+                for added in new_outputs
+                    .iter()
+                    .filter(|&output| !windows.contains_key(output))
+                    .collect::<Vec<_>>()
+                {
+                    let monitor = monitors
+                        .iter()
+                        .find(|monitor| monitor.connector().as_deref() == Some(added))
+                        .context("unknown monitor")?
+                        .clone();
+
+                    let mut controller = AppModel::builder().launch(monitor).detach();
+                    let window = controller.widget();
+                    app.add_window(window);
+                    window.set_visible(true);
+                    controller.detach_runtime();
+
+                    ensure!(
+                        windows.insert(added.into(), controller).is_none(),
+                        "nonexistent element exists"
+                    );
+                }
+            }
+            event @ (AppInput::Time(_) | AppInput::Layout(_)) => {
+                // TODO store SHARED state AND send update events ONLY if needed
+
+                for controller in windows.values() {
+                    // XXX: Blocking call??
+                    controller.sender().emit(event.clone());
+                }
+            }
+        }
+    }
 }
 
 fn main() -> glib::ExitCode {
