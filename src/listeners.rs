7250a247de0cf14bcd97e2bbab72c6c54780bf1a
Yuki Sireneva <yuki.utk8g@gmail.com>
2024-10-14 06:07:25 +0300

    listeners/sway: Reintroduce xkb_layout_switch action with correct state

--- src/listeners.rs
+++ src/listeners.rs
@@ -11,7 +11,7 @@ mod upower;
 
 pub fn start(tx: mpsc::UnboundedSender<AppInput>, state: Arc<RwLock<AppState>>) {
     trace!("Spawning listeners...");
-    tokio::spawn(sway::start(tx.clone(), Arc::clone(&state)));
+    relm4::spawn_local(sway::start(tx.clone(), Arc::clone(&state)));
     tokio::spawn(time::start(tx.clone(), Arc::clone(&state)));
     tokio::spawn(sound::start(tx.clone(), Arc::clone(&state)));
     relm4::spawn_local(upower::start(tx.clone(), Arc::clone(&state)));
--- src/listeners/sway.rs
+++ src/listeners/sway.rs
@@ -2,7 +2,9 @@ use crate::bar::AppInput;
 use crate::state::AppState;
 use eyre::{bail, Context, Result};
 use futures_lite::stream::StreamExt;
-use log::{info, trace};
+use gtk4::prelude::ActionMapExt;
+use log::{error, info, trace};
+use relm4::gtk::{gio, glib};
 use std::sync::{Arc, RwLock};
 use swayipc_async::{Connection, Event, EventType};
 use tokio::sync::mpsc;
@@ -29,6 +31,51 @@ pub async fn start(
         ])
         .await
         .context("subscribe to events")?;
+
+    let (command_tx, mut command_rx) = mpsc::unbounded_channel();
+    tokio::spawn(async move {
+        let mut conn = Connection::new()
+            .await
+            .context("initial connection")
+            .unwrap();
+        while let Some(payload) = command_rx.recv().await {
+            trace!("Requesting {payload}...");
+            let res = conn.run_command(&payload).await;
+            if res.is_err() {
+                error!("got {res:?} in response to {payload}");
+            }
+        }
+    });
+
+    let action_switch_layout = gio::SimpleAction::new_stateful(
+        "xkb_switch_layout",
+        Some(glib::VariantTy::INT32),
+        &0.into(),
+    );
+    let command_tx_ = command_tx.clone();
+    action_switch_layout.connect_change_state(move |_action, value| {
+        log::trace!("Switch layout action triggered with new value {value:?}");
+        let Some(value) = value else { return };
+        let Some(value) = value.get::<i32>() else {
+            return;
+        };
+        command_tx_
+            .send(format!("input type:keyboard xkb_switch_layout {value}"))
+            .expect("send command");
+    });
+    relm4::main_application().add_action(&action_switch_layout);
+
+    let (new_tx, mut rx) = mpsc::unbounded_channel();
+    relm4::spawn_local(async move {
+        while let Some(event) = rx.recv().await {
+            trace!("Forwarding event {event:?}");
+            if let AppInput::Layout(idx) = event {
+                action_switch_layout.set_state(&idx.into());
+            }
+            tx.send(event).expect("forward event");
+        }
+    });
+    let tx = new_tx;
 
     info!("Sway listener ready");
 
